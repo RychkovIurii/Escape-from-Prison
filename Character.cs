0f5fbe3450d81d1ddd9182e9f42733512ccbb4d3
Rychkov Iurii <rychkoviurii@hotmail.com>
2024-05-15 23:03:52 +0300

    Character and Items

---
+++ Character.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Prison
+{
+    public class Character
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Dictionary<string, Action<Player>> Actions { get; set; }
+
+        public Character(string name, string description)
+        {
+            Name = name;
+            Description = description;
+            Actions = new Dictionary<string, Action<Player>>();
+        }
+    }
+}
+
---
+++ Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Prison
+{
+    public class Item
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+
+        public Item(string name, string description, int quantity = 1)
+        {
+            Name = name;
+            Description = description;
+            Quantity = quantity;
+        }
+    }
+}
+
