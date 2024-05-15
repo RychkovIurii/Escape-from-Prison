16d26023abbcd4d5c725e0e92e9da9041ed164c8
Rychkov Iurii <rychkoviurii@hotmail.com>
2024-05-15 23:04:15 +0300

    Main

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Prison
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game();
+            game.Play();
+        }
+    }
+}
+
